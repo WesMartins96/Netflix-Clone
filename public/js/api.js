797d484991c5ffd77e58f5da0fad5018e235cf38
Wesley Martins <wesley.mlopes@castgroup.com>
2022-12-17 10:54:31 -0300

    implementacao da API no projeto

---
+++ public/js/api.js
@@ -0,0 +1,13 @@
+//Usei a API themoviedb.org
+
+//chave da API
+let api_key = "6a13cca23b6aff72941441ed6aab2481";
+
+// URL que traz as imagens
+let img_url = "https://image.tmdb.org/t/p/w500";
+
+//Traz os generos
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+//Faz a descoberta dos filmes
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
